191083f47a2d2f5549fd1d31d2084057deac0e25
Jong44 <mat.skak45@gmail.com>
2024-11-25 21:25:49 +0700

    update matkul page & integrate dummy data

---
+++ src/api/setupProxy.js
@@ -0,0 +1,11 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//     app.use(
+//         'https://dummy-kampus.syaifur.id',
+//         createProxyMiddleware({
+//             target: 'http://localhost:5173',
+//             changeOrigin: true,
+//         })
+//     );
+// };
